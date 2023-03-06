fa5b88ce0d5efc64c4d7760cd0b50211dbde969c
Marcelo Leiva <leivaskt@gmail.com>
2023-03-06 15:49:14 -0300

    termine ejer 3 se me complico entender la funcion confirm

---
+++ js/Ejer3.js
@@ -0,0 +1,5 @@
+
+    while(confirm('¿Desea Continuar?') == true){
+    let text = prompt('ingrese las cadenas a concatenar cuando desee terminar pulse CANCELAR');
+    document.write(text,'.-')
+  } 
